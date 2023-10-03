acc2b70d2b662d9cc2527f76b0cf9017f0f3bcf2
Weissey <kelvinchangw@gmail.com>
2023-10-02 22:10:47 -0700

    Add various scroll functions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, 1);
+});
+
+
 const myLibrary = [];
 
 function Book() {
@@ -7,3 +17,10 @@ function Book() {
 function addBookToLibrary() {
     
 }
+
+const main = document.getElementById("main");
+const beginButton = document.getElementById("begin-btn");
+
+beginButton.addEventListener(("click"), () => {
+    main.scrollIntoView({behavior: "smooth"})
+});
