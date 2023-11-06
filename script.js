9c6bd3767e46de87a7fa2c0f2ce5ba195192ccca
Weissey <kelvinchangw@gmail.com>
2023-11-06 12:18:38 -0800

    Add form validity check

--- script.js
+++ script.js
@@ -19,6 +19,30 @@ function addBookToLibrary() {
     
 }
 
+const newBookForm = document.getElementsByClassName("add-book-form");
+
+// Checks if form is valid before adding book to library
+function checkForm() {
+    const inputs = document.querySelectorAll("input");
+    let allValid = true;
+
+    for(let input of inputs) {
+        if(input.value === "") {
+            input.classList.add("invalid");
+            allValid = false;
+        }
+        else {
+            input.classList.remove("invalid");
+        }
+    }
+
+    if(allValid) addBookToLibrary();
+}
+
+// Listens for add new book button
+const addNewBookBtn = document.getElementById("add-book-button");
+addNewBookBtn.addEventListener("click", checkForm);
+
 // Smooth scroll to main container that has sidebar and book grid
 const main = document.getElementById("main");
 const beginButton = document.getElementById("begin-btn");
